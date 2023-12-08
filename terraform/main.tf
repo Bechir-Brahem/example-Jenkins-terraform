b4b6cd46006765e19ba1eddab84184c9b160fe9d
dalideco <dalissj1@gmail.com>
2023-12-08 19:01:21 +0100

    remove repo_url env variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-variable "repo_url" {
-  description = "The URL of the Git repository"
-  type        = string
-}
-
 variable "gcp_project_id" {
   description = "The GCP project ID"
   type        = string
