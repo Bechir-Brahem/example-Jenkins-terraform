9437db34109afb55dec44da3b2abeb1b7b911eb0
dalideco <dalissj1@gmail.com>
2023-12-08 23:50:08 +0100

    mergin init, plan and apply

--- terraform/backend-prod.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "terraform-state-prod-gl5"
-    prefix  = "terraform/state/prod"
-  }
-}
--- terraform/backend-test.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "terraform-state-test-gl5"
-    prefix  = "terraform/state/test"
-  }
-}
