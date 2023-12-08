b5ef1d33f1c4a6cf987268be06b9055d4d7cceca
dalideco <dalissj1@gmail.com>
2023-12-08 22:32:15 +0100

    remove base64 decode

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = jsondecode(base64decode(var.gcp_sa_key))
+  credentials = jsondecode(var.gcp_sa_key)
   project     = var.gcp_project_id
   region      = "europe-west3"
   zone        = "europe-west3-a"
