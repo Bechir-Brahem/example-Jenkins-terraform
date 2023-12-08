1b829ce75b4bcf39d2b42f6616d38ed1c09b4642
dalideco <dalissj1@gmail.com>
2023-12-08 19:50:52 +0100

    updating outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
-output "instance_ip" {
-  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+output "cloud_run_service_url" {
+  value     = google_cloud_run_service.default.status[0].url
+  description = "The URL of the deployed Cloud Run service"
 }
