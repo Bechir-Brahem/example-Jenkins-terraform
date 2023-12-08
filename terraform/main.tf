beffd5905492dac2da3ee5f83cff29d2d406ab82
dalideco <dalissj1@gmail.com>
2023-12-08 22:53:58 +0100

    updating main tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,31 @@
-resource "google_cloud_run_service" "default" {
-  name     = "django-app-service-test"
-  location = "europe-west3"
+resource "google_cloud_run_v2_service" "default" {
+  name     = "django-app"
+  location = var.region
+  client   = "terraform"
+  project  = var.project_id
 
   template {
-    spec {
-      containers {
-        image = "gcr.io/${var.gcp_project_id}/your-app-test"  # Replace with your image path
+    containers {
+      image = "gcr.io/${var.gcp_project_id}/your-app-test"
         ports {
           container_port = 8000
-        }   
-      }
-    }
+        }    
+}
+  }
+  traffic {
+    percent = 100
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+
   }
 
-  traffic {
-    percent         = 100
-    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-  }
 }
 
-# Set the Cloud Run service to be publicly accessible
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = var.gcp_project_id
-  name     = google_cloud_run_service.default.name
+resource "google_cloud_run_v2_service_iam_member" "noauth" {
+  project = "tp-4-gl5"
+  location = google_cloud_run_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
+
 
