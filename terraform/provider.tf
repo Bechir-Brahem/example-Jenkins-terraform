0cad24c986816cc7bd400067101beb505f22806f
dalideco <dalissj1@gmail.com>
2023-12-08 22:39:54 +0100

    passing gcp_sa_key as string

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = jsondecode(var.gcp_sa_key)
+  credentials = var.gcp_sa_key
   project     = var.gcp_project_id
   region      = "europe-west3"
   zone        = "europe-west3-a"
